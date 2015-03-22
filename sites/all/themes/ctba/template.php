f20d677abfdebca9f43742beddc3e254961f88ca
Kirk Daniels <kirk@omnistudio.co.za>
2015-03-22 20:08:00 +0200

    stuff

--- sites/all/themes/ctba/template.php
+++ sites/all/themes/ctba/template.php
@@ -1,8 +1,10 @@
 <?php
-    
-    function ctba_preprocess_node(&$variables) {
-          $variables['theme_hook_suggestions'][] = 'node__' . $variables['type'] . '__' . $variables['view_mode'];
-        }
 
-    drupal_add_js(drupal_get_path('theme', 'ctba') .'/assets/js/libraries/modernizr-2.6.2.min.js');
-    drupal_add_js(drupal_get_path('theme', 'ctba') .'/assets/js/scripts/app.js');
+/**
+ * @file
+ * template.php
+ */
+
+
+
+?>
