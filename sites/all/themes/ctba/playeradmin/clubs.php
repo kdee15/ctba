e2847e1690c9fa7238a5c09a52edc99b74334aaa
Kirk Daniels <kirk@omnistudio.co.za>
2015-03-25 07:47:45 +0200

    :S

---
+++ sites/all/themes/ctba/playeradmin/clubs.php
@@ -0,0 +1,48 @@
+<html>
+<body>
+    
+    <?php include('includes/head.inc'); ?>
+    <?php include('includes/header.inc'); ?>
+
+        <!-- C. CONTENT AREA -->    
+
+        <section id="mast">
+            
+            <div class="container">
+
+                <h1>Clubs</h1>
+                
+                <section class="clubs">
+                
+                    <article class="club__item">
+                    
+                        <section class="club__item-header">
+                        
+                            <h2>Club Name</h2>
+                        
+                        </section>
+                        <div class="wrapper">
+                        
+                            <ul>
+                            
+                                <li><a><span>VIEW</span></a></li>
+                                <li><a><span>DELETE</span></a></li>
+                            
+                            </ul>
+                        
+                        </div>
+                    
+                    </article>
+                
+                </section>
+
+            </div>
+
+        </section>
+
+        <!-- C. END -->    
+
+    <?php include('includes/footer.inc'); ?>
+
+</body>
+</html>
---
+++ sites/all/themes/ctba/playeradmin/main.php
@@ -0,0 +1,35 @@
+<html>
+<body>
+    
+    <?php include('includes/head.inc'); ?>
+    <?php include('includes/header.inc'); ?>
+
+        <!-- C. CONTENT AREA -->    
+
+        <section id="mast">
+            
+            <div class="container">
+
+                <h1>Welcome</h1>
+                
+                <ul>
+                
+                    <li><a href="leagues.php"><span>Leagues</span></a></li>
+                    <li><a href="clubs.php"><span>Clubs</span></a></li>
+                    <li><a href="teams.php"><span>Teams</span></a></li>
+                    <li><a href="members.php"><span>Members</span></a></li>
+                    <li><a href="roles.php"><span>Roles</span></a></li>
+                
+                </ul>
+                
+
+            </div>
+
+        </section>
+
+        <!-- C. END -->    
+
+    <?php include('includes/footer.inc'); ?>
+
+</body>
+</html>
