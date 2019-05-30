b513f01112eeff2db25559e1e53a72e672cb6a02
kirk <kdee15@gmail.com>
2019-05-31 00:20:38 +0200

    soma

--- archive.php
+++ archive.php
@@ -1,63 +1,63 @@
-<?php get_template_part( 'inc/page-header' ); ?>
+<?php
+
+/*
+Template Name: Archive page
+*/
+
+get_header(); ?>
 
 <!-- C. WORK AREA +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
-    <!-- C.2. SITE MAST ------------------------------------------------------------------------------------------- -->
+<!-- C.2. SITE MAST ------------------------------------------------------------------------------------------- -->
 
-    <section class="page archive" id="mast">
 
-        <!-- C.2.1. INTRO ----------------------------------------------------------------------------------------- -->
+<?php get_template_part( 'inc/page-header' ); ?>
 
-        <?php get_template_part( 'section-front' ); ?>
+<?php get_template_part( 'inc/navigation-page' ); ?>
 
-        <!-- C.2.1. End ------------------------------------------------------------------------------------------- -->
-        
-        <!-- C.2.2. SLIDER AREA ----------------------------------------------------------------------------------- -->
-        
-        <div class="wrapper">
-        
-            <!-- C.2.2.1. ABOUT US -------------------------------------------------------------------------------- -->
-            
-            <section class="content">
-            
-                <div class="container">
-                    
-                    <h1>Blog Archive</h1>
-                
-                    <?php
-                        $args=array(
-                          'post_type' => 'post',
-                          'post_status' => 'publish'
-                        );
-                        $my_query = null;
-                        $my_query = new WP_Query($args);
+<!-- C.1. END ----------------------------------------- -->
 
-                        if( $my_query->have_posts() ) {
-                          while ($my_query->have_posts()) : $my_query->the_post(); ?>
+<main class="p-main page default-page">
 
-                            <?php the_title() ?>
+  <div class="container" id="container">
 
-                            <?php
-                          //the_excerpt();
-                          endwhile;
-                        }
+    <!-- C.2. SECTIONS -------------------------------- -->
 
-                        wp_reset_query();  // Restore global post data stomped by the_post().
-                    ?>
-                
-                </div>
-                
-            </section>
-            
-            <!-- C.2.2.1. END ------------------------------------------------------------------------------------- -->
-        
-        </div>
-        
-        <!-- C.2.2. End ------------------------------------------------------------------------------------------- -->
+    <?php
+    $args=array(
+      'post_type' => 'post',
+      'post_status' => 'publish'
+    );
+    $my_query = null;
+    $my_query = new WP_Query($args);
 
-    </section>
-    
-    <!-- C.2. END ------------------------------------------------------------------------------------------------- -->
+    if( $my_query->have_posts() ) {
+      while ($my_query->have_posts()) : $my_query->the_post(); ?>
+
+        <?php the_title() ?>
+
+      <?php
+        //the_excerpt();
+      endwhile;
+    }
+
+    wp_reset_query();  // Restore global post data stomped by the_post().
+    ?>
+
+    <!-- C.2. END ------------------------------------- -->
+
+  </div>
+
+  <!-- C.3. FOOTER  ----------------------------------- -->
+
+  <?php get_footer(); ?>
+
+  <!-- C.3. END --------------------------------------- -->
+
+</main>
+
+
+<!-- C.2. END ------------------------------------------------------------------------------------------------- -->
 <?php get_footer(); ?>
 <!-- C. END +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ -->
 
